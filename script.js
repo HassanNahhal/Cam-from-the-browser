c8d1589c93a9bbcde3f370d7a4b3e34af5697997
HassanNahhal <hassannahhal@Hassans-MacBook-Pro.local>
2018-01-11 23:32:52 -0500

    Added download

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var fileChooser = document.getElementsByTagName('camera');
+var fileChooser = document.getElementsByTagName('input')[0];
 var content = document.getElementById('content');
+//var imageElement = document.getElementById('imageId');
 
 if (typeof window.FileReader === 'undefined') {
     content.className = 'fail';
@@ -11,21 +12,20 @@ fileChooser.onchange = function (e) {
 
     var file = fileChooser.files[0],
         reader = new FileReader();
-        
+
     reader.onerror = function (event) {
         content.innerHTML = "Error reading file";
     }
 
     reader.onload = function (event) {
         var img = new Image();
-        /*
+
         // files from the Gallery need the URL adjusted
         if (event.target.result && event.target.result.match(/^data:base64/)) {
             img.src = event.target.result.replace(/^data:base64/, 'data:image/jpeg;base64');
         } else {
             img.src = event.target.result;
         }
-        */
 
         // Guess photo orientation based on device orientation, works when taking picture, fails when loading from gallery
         if (navigator.userAgent.match(/mobile/i) && window.orientation === 0) {
@@ -35,21 +35,29 @@ fileChooser.onchange = function (e) {
             img.width = 400;
         }
 
+        //imageElement.appendChild(img);
         content.innerHTML = '';
         content.appendChild(img);
-        alert(img);
     };
-    
+
     reader.readAsDataURL(file);
 
     return false;
 }
 
-saveImage= function (base64, fileName) {
-
-    var link = document.createElement("a");
-
-    link.setAttribute("href", base64);
-    link.setAttribute("download", fileName);
-    link.click();
+saveImage = function (fileName) {
+    var base64 = getBase64Image(document.getElementById("content")).onload();
+    link.setAttribute("href", imgData);
+    link.setAttribute("download", fileName); link.click();
 }
+
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+    var dataURL = canvas.toDataURL("image/png");
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+  }
+  
